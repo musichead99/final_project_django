623cbc692945107c9365fb0b73b85d8bd485165a
musichead99 <musichead99@naver.com>
2022-06-17 01:17:16 +0900

    게시글 목록, 댓글 출력 기능 작성

--- myPage/urls.py
+++ myPage/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'myPage'
+
 urlpatterns = [
-    path("",views.index),
+    path('', views.index, name='index'),
+    path('<int:post_id>/', views.detail, name='detail'),
+    path('comment/create/<int:post_id>/', views.comment_create, name='comment_create'),
 ]
--- myPage/views.py
+++ myPage/views.py
@@ -1,5 +1,18 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+from django.utils import timezone
+from .models import Post
 
 def index(request) :
-    return HttpResponse("안녕하세요 myPage에 오신 걸 환영합니다.")
+    post_list = Post.objects.order_by('-create_date')
+    context = {'post_list' : post_list}
+    return render(request, 'myPage/post_list.html', context)
+
+def detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    context = {'post' : post}
+    return render(request, 'myPage/post_detail.html', context)
+
+def comment_create(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    post.comment_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('myPage:detail', post_id=post.id)
