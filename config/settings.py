55c7d36f37ea8293a2cd0b94f3af39fdb4004569
musichead99 <musichead99@naver.com>
2022-06-17 00:24:56 +0900

    ORM model(Post, Comment) 생성

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'myPage.apps.MypageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- myPage/models.py
+++ myPage/models.py
@@ -1,3 +1,14 @@
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    audio = models.CharField(max_length=200)
+    create_date = models.DateTimeField()
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, default='')
+    content = models.TextField()
+    create_date = models.DateTimeField()
