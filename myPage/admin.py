e01cdd6ccbb347fa1ace08bed5dd1ab2a2438ed8
musichead99 <musichead99@naver.com>
2022-06-17 00:40:23 +0900

    슈퍼유저 기능 추가

--- myPage/admin.py
+++ myPage/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+
+admin.site.register(Post, PostAdmin)
--- myPage/models.py
+++ myPage/models.py
@@ -5,8 +5,11 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    audio = models.CharField(max_length=200)
+    audio = models.CharField(max_length=200, blank=True, default='')
     create_date = models.DateTimeField()
+    
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, default='')
